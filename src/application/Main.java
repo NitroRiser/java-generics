d09071d3d9ab12cb30c4dec64a61dcd1ad355751
NitroRiser <jonathanfariastech@gmail.com>
2022-08-17 18:45:17 +0000

    set

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,29 @@
 package application;
 
-import entities.Client;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Client c1 = new Client("Maria", "maria@gmail.com");
-        Client c2 = new Client("Maria", "maria@gmail.com");
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 
-        String s1 = "teste";
-        String s2 = "teste";
+        //union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
 
-        System.out.println(c1.hashCode());
-        System.out.println(c2.hashCode());
-        System.out.println(c1.equals(c2));
-        System.out.println(c1 == c2);
-        System.out.println(s1 == s2);
+        //intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
+
+        //difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
     }
 }
