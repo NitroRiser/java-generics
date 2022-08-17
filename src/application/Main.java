e68cc6665b0154d44531c0550e49ab0d3294d48e
NitroRiser <jonathanfariastech@gmail.com>
2022-08-17 18:03:00 +0000

    bounded wildcards

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,32 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> myInts = Arrays.asList(5, 2, 10);
-        printList(myInts);
 
-        List<String> myStr = Arrays.asList("Maria", "Alex", "Bob");
-        printList(myStr);
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+        List<Object> myObjs = new ArrayList<Object>();
+
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
     }
 
-    public static void printList(List<?> list){
-        for (Object obj : list){
-            System.out.println(obj);
+    public static void copy(List<? extends Number> source, List<? super Number> target) {
+        for (Number number : source) {
+            target.add(number);
         }
+    }
+
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.print(obj + "");
+        }
+
     }
 }
